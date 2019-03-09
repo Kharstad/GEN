44aa7b68f45c169f36d0cbcad410a0a58877b7cf
Enos Fernandes <47147026+Kharstad@users.noreply.github.com>
2019-03-09 13:56:20 -0300

    Update pagina

--- cadastrar_cliente.php
+++ cadastrar_cliente.php
@@ -3,10 +3,10 @@ include("conexao.php");
 if(isset($_POST["cadastrar"])){
     
     $nome = $_POST["nome"];
-    $empresa = $_POST["empresa"];
     $sobrenome = $_POST["sobrenome"];
     $login = $_POST["login"];
     $senha = $_POST["senha"];
+    $empresa = $_POST['empresa'];
     $tipo_logradouro = $_POST["tipo_logradouro"];
     $logradouro = $_POST["logradouro"];
     $numero = $_POST["numero"];
@@ -15,30 +15,25 @@ if(isset($_POST["cadastrar"])){
     $estado = $_POST["estado"];
     $telefone = $_POST["telefone"];
     $email = $_POST["email"];
-   
-    
-      
-    $sqlcadalunoend = "insert into endereco (cod_end,tipo_logradouro,logradouro,numero,bairro,cep,estado)
-                    values(null,'{$tipo_logradouro}','{$logradouro}','{$numero}','{$bairro}','{$cep}','{$estado}')";
+        
+    $sqlend = "insert into endereco (cod_end,tipo_logradouro,logradouro,numero,bairro,cep,estado) values(null,'{$tipo_logradouro}','{$logradouro}','{$numero}','{$bairro}','{$cep}','{$estado}')";
 
-    $sqlcadcontato = "insert into contato (cod_contato,tel,email) values (null,'{$telefone}','{$email}')";
+    $sqlcontato = "insert into contato (cod_contato,tel,email) values (null,'{$telefone}','{$email}')";
     
-    $sqlcadempresa = "insert into empresa (cod_empresa,nome_empresa,cod_end,cod_contato) values ('{$empresa}')";
+    $sqlempresa = "insert into empresa (cod_empresa,nome_empresa) values (null,'{$empresa}')";
 
-    $sqlcadclie = "insert into cliente (nome_clie,sobrenome_clie,login,senha,cod_end,cod_contato,cod_empresa)
-                    values ('{$nome}','{$sobrenome}','{$login}','{$senha}')";
+    $sqlclie = "insert into cliente (nome_clie,sobrenome_clie,login,senha,cod_end,cod_contato) values ('{$nome}','{$sobrenome}','{$login}','{$senha}')";
     
-    if(mysqli_query($con, $sqlcadalunoend)){
-        if(mysqli_query($con , $sqlcadcontato)){
-          if(mysqli_query($con, $sqlcadclie)){
+    if(mysqli_query($con, $sqlend)){
+        if(mysqli_query($con, $sqlcontato)){          
+    if(mysqli_query($con, $sqlempresa)){
+        if(mysqli_query($con, $sqlclie)){
             echo "Cadastro efetuado com sucesso!";
-
     }else{
         echo "Não foi possível efetuar o cadastro";
     }
     }
     }
-    
-     
-}
+    }
+}     
 ?>
